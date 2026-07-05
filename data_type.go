b7a34183ba0de1d492cdde0f33fa90bc05a5d5af
harshsingh262”git config --global user.name harshsingh262” <harshsingh84562@gmail.com>
2026-07-05 14:44:35 +0530

    Learn data types

---
+++ data_type.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var a int = 10
+	var b int = 20
+	fmt.Println("The sum of a and b is: ", a+b)
+
+	num1 := 15
+	num2 := 25
+	fmt.Printf("The product of num1 and num2 is: %d\n", num1*num2)
+}
